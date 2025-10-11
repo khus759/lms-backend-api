cdbf36b3d4175b9cf67ac287ba7b263274a78938
khus759 <khushikumarigupta@email.com>
2025-10-11 09:39:17 +0530

    category by teacher update

--- routers/teachers/teachers.py
+++ routers/teachers/teachers.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+# from fastapi import APIRouter
+# import json
+# from config import TEACHERS_FILE
+
+# router = APIRouter()
+
+# @router.get("/teachers")
+# def get_teachers():
+#     with open(TEACHERS_FILE, "r", encoding="utf-8") as file:
+#         data = json.load(file)
+#     return data
+
+
+
+from fastapi import APIRouter, HTTPException
 import json
 from config import TEACHERS_FILE
 
@@ -9,3 +23,18 @@ def get_teachers():
     with open(TEACHERS_FILE, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
+
+
+@router.get("/teachers/{teacher_id}")
+def get_teacher_by_id(teacher_id: int):
+ 
+    with open(TEACHERS_FILE, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    for category, teachers in data.items():
+        for teacher in teachers:
+            if teacher.get("id") == teacher_id:
+                teacher["category"] = category
+                return teacher
+
+    raise HTTPException(status_code=404, detail="Teacher not found")
